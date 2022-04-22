421a73210a528c56f05f6ee5bf4a73565a2b0c29
EC2 Default User <ec2-user@ip-172-31-10-73.ec2.internal>
2022-04-22 08:06:56 +0000

    ADD addresses

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,42 @@
 class Public::AddressesController < ApplicationController
+  
+  def new
+    @address = Address.new
+  end
+  
+  def create
+    @address = Address.new(address_params)
+    @address.end_user_id = current_end_user.id
+    @address.save
+    redirect_to '/addresses'
+  end
+  
   def index
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
+  
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path(@address.id)
+  end
+  
+  def destroy
+     @address = Address.find(params[:id])
+     @address.destroy
+     @address = current_end_user.address
+     redirect_to addresses_path
+  end
+    
+  private
+  
+  def address_params
+    params.require(:address).permit(:postcode, :address, :name)
+  end
+  
 end
