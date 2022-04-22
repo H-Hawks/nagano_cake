3dbd61df73d60635062814c225eb990b425accb0
EC2 Default User <ec2-user@ip-172-31-15-118.ap-northeast-1.compute.internal>
2022-04-22 09:02:40 +0000

    ADD public_products and pagination

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,12 @@
 class Public::ProductsController < ApplicationController
   def index
+   @products = Product.page(params[:page])
   end
 
   def show
+  @product = Product.find(params[:id])
   end
+  
+  
+
 end
