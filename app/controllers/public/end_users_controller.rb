472932cb159847d1862ec52536d1338ea402cb4f
EC2 Default User <ec2-user@ip-172-31-10-73.ec2.internal>
2022-04-23 06:30:38 +0000

    ADD end_user-status

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,7 +1,15 @@
 class Public::EndUsersController < ApplicationController
   def show
+    @end_user = EndUser.find(params[:id])
   end
 
   def edit
   end
+  
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:)
+  end
+  
 end
