5bebdd417ce81828c0c2c8937168c69f9333ea3c
EC2 Default User <ec2-user@ip-172-31-15-118.ap-northeast-1.compute.internal>
2022-04-24 10:26:10 +0000

    ADD count

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,11 +1,12 @@
 class Public::ProductsController < ApplicationController
-  def index
-   @products = Product.page(params[:page])
-  end
+ def index
+ @total_products = Product.all
+ @products = Product.page(params[:page])
+ end
 
-  def show
-  @product = Product.find(params[:id])
-  end
+ def show
+ @product = Product.find(params[:id])
+ end
   
   
 
