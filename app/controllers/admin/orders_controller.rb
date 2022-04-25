a20b459c4ec49c899f253503782fa77e0a7e843f
EC2 Default User <ec2-user@ip-172-31-10-73.ec2.internal>
2022-04-25 11:22:11 +0000

    ADD history

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,19 @@
 class Admin::OrdersController < ApplicationController
+  
+  before_action :authenticate_admin!
+  
   def index
+    @orders = Order.all
   end
 
   def show
+    # @orders = Order.find(params[:id])
   end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:end_user_id,:postcode,:address,:name,:shipping_fee,:bill,:payment_method,:order_status)
+  end
+  
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,7 +1,14 @@
 class OrderDetail < ApplicationRecord
   belongs_to :end_users
   belongs_to :orders
-
+  belongs_to :product
+  
+  enum production_status: {
+    製作不可: 0,
+    製作待ち: 1,
+    製作中: 2,
+    製作完了: 3,
+  }
   def subtotal
     product.add_tax_price * quantity
   end
