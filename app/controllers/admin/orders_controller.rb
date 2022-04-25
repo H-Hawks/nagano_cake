c49c4b2545023f98b4affd0e18cc4cfffde9ff59
EC2 Default User <ec2-user@ip-172-31-10-73.ec2.internal>
2022-04-25 13:24:20 +0000

    ADD history

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,13 +7,13 @@ class Admin::OrdersController < ApplicationController
   end
 
   def show
-    # @orders = Order.find(params[:id])
+    @orders = Order.find(params[:id])
   end
   
   private
   
   def order_params
-    params.require(:order).permit(:end_user_id,:postcode,:address,:name,:shipping_fee,:bill,:payment_method,:order_status)
+    params.require(:order).permit(:postcode,:address,:name,:quantity,:shipping_fee,:bill,:payment_method,:order_status)
   end
   
 end
