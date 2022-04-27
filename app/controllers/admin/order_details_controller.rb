0d25b2fa6d4cbbc432e04d9110feae213a271d7b
EC2 Default User <ec2-user@ip-172-31-10-73.ec2.internal>
2022-04-27 17:53:01 +0000

    ADD

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,9 +3,23 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
     
   def update
-    @order_detail = OrderDetail.find(params[:id])
-    @order_detail.update(order_detail_params)
-    redirect_to request.referer
+    @order = Order.find(params[:order_id])
+    @order_detail = @order.order_details.find(params[:id])
+    @order_details = @order.order_details
+    if @order_detail.update(order_detail_params)
+      if  @order_detail.production_status == "in_productions"
+          @order.order_status = "production"
+          @order.save
+          redirect_to request.referer
+      end
+    elsif @order_details.each do |order_detail|
+      if order_detail.production_status == "production_completed"
+          @order.order_status = "ready_to_ship"
+          @order.save!
+          redirect_to request.referer
+      end
+      end
+    end
   end
     
     
@@ -13,6 +27,10 @@ class Admin::OrderDetailsController < ApplicationController
     
   def order_detail_params
     params.require(:order_detail).permit(:name,:tax_price,:quantity,:production_status,:product_id,:order_id)
+  end
+  
+  def order_params
+    params.require(:order).permit(:order_status)
   end
     
 end
